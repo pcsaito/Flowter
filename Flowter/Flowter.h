0773989e75e538cef23eaaaa5a027c8d9d0c73ee
Paulo Cesar Saito Lopes <paulostool@gmail.com>
2018-05-17 17:19:20 -0300

    Initial Commit

---
+++ Flowter/Flowter.h
@@ -0,0 +1,19 @@
+//
+//  Flowter.h
+//  Flowter
+//
+//  Created by Paulo Cesar Saito on 17/05/18.
+//  Copyright © 2018 Zazcar. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Flowter.
+FOUNDATION_EXPORT double FlowterVersionNumber;
+
+//! Project version string for Flowter.
+FOUNDATION_EXPORT const unsigned char FlowterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Flowter/PublicHeader.h>
+
+
